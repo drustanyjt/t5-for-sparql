00e1ae8ff6f60ad553bd087802af3fc58b60e5c2
Drustan DSO@Titan <yjunteng@dso.org.sg>
2023-06-14 16:54:41 +0800

    trained t5-base

---
+++ scripts/serve.sh
@@ -0,0 +1,2 @@
+python /home/yjunteng/t5-for-sparql/src/serve.py /home/yjunteng/t5-for-sparql/models/text2sparql-t5-small-lcquad2/checkpoints/checkpoint_56000.pth /home/yjunteng/t5-for-sparql/data/lcquad2/vocab.txt --gpu 1
+
--- scripts/train_text2sparql.sh
+++ scripts/train_text2sparql.sh
@@ -1,1 +1,2 @@
 papermill text2sparql.ipynb papermills/text2sparql.papermill.ipynb --no-progress-bar --stdout-file -
+
