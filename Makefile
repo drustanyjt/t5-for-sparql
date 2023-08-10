c99fb3f5620b097e00dbdd3f212c62eff9a40e00
Drustan DSO@Titan <yjunteng@dso.org.sg>
2023-08-10 17:17:54 +0800

    removed extra files

--- Makefile
+++
@@ -1,2 +0,0 @@
-train-long:
-	python src/train_LongT5.py data/lcquad2_train.json --saveto models/longt5 --epochs 50
--- pipeline/__init__.py
+++
@@ -1,7 +0,0 @@
-from pipeline.BaseAnnotator import BaseAnnotator
-from pipeline.BaseConverter import BaseConverter
-from pipeline.BaseLinker import BaseLinker
-from pipeline.Falcon2Linker import Falcon2Linker
-from pipeline.SerialAnnotator import SerialAnnotator
-from pipeline.T5Converter import T5Converter
-from pipeline.WikidataObject import WikidataObject
